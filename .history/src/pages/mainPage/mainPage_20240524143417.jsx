60f7b4227b0c6a287381e44333848b38bd34e9a5
Azizbek <azizbekkarimov@alatoo.edu.kg>
2024-05-29 11:32:55 +0600

    added many things

---
+++ .history/src/pages/mainPage/mainPage_20240524143417.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './mainPage.scss';
+import Problems from '../../components/problems/problems';
+import Schedule from '../../components/schedule/Schedule';
+
+const MainPage = () => {
+  return (
+    <div className='mainPage'>
+        <div className="content container">
+            <div className="main_text">MAKE COMPUTER VISION AND ARTIFICIAL INTELLIGENCE SYSTEMS CLOSER TO STUDENTS</div>
+            <div className="logs">
+            <button></button>
+            </div>
+        </div>
+        <Problems />
+        <Schedule/>
+    </div>
+  )
+}
+
+export default MainPage;
---
+++ .history/src/pages/mainPage/mainPage_20240524143605.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './mainPage.scss';
+import Problems from '../../components/problems/problems';
+import Schedule from '../../components/schedule/Schedule';
+
+const MainPage = () => {
+  return (
+    <div className='mainPage'>
+        <div className="content container">
+            <div className="main_text">MAKE COMPUTER VISION AND ARTIFICIAL INTELLIGENCE SYSTEMS CLOSER TO STUDENTS</div>
+            <div className="logs">
+            <button className='login'>LOG IN</button>
+            <button>SIGN IN</button>
+            </div>
+        </div>
+        <Problems />
+        <Schedule/>
+    </div>
+  )
+}
+
+export default MainPage;
